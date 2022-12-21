53ce0e88870bc92681625274f4c5a634306aae8d
DG Yoon <douglasyoon9@gmail.com>
2022-12-21 11:03:55 +0900

    기본 세팅 및 리뷰 기능 - 기본 세팅 - 리뷰 기능 작업 중

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,5 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:3000",
   baseURL: "http://haeji.mawani.kro.kr:8181",
-  params: {},
 });
 export default instance;
--- src/components/ReviewItem.js
+++ src/components/ReviewItem.js
@@ -2,10 +2,13 @@ import "../style/review.css";
 
 const ReviewItem = (props) => {
   const reviewData = props.reviewData;
+  const star = [...Array(reviewData.reviewSroce)].map((n, i) => <span key={i}>&#9733;</span>);
+
   return (
     <li className="list-group-item review-item">
       <p className="review-info">
-        {reviewData.reviewSroce}|{reviewData.memberId}|{reviewData.reviewDate}
+        <span>{star}</span>
+        <span>{reviewData.memberId}</span> <span>{reviewData.reviewDate}</span>
       </p>
       <p className="review-content">{reviewData.reviewContent}</p>
     </li>
