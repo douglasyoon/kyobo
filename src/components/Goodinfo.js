857b9f6c9905e3b7bd2b235310f14c6f4ab0ea33
DG Yoon <douglasyoon9@gmail.com>
2022-12-21 16:41:58 +0900

    전체 스타일 수정

--- src/components/Goodinfo.js
+++ src/components/Goodinfo.js
@@ -1,48 +1,29 @@
-
 import "../style/GoodInfo.css";
 import "../style/TopButton.css";
-
-import { Link } from "react-router-dom";
-
 const Goodinfo = (props) => {
-  // const[Auther,]
-
   const detailInfo = props.detailInfo;
   const scrollToTop = () => {
     window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })}
-
-
-  // const Auther = detailInfo.autherName
-
-  // if(Auther.length >= 10){
-  //   return Auther.substr(0,10)+"...";
-  // }
-
-  //  if(detailInfo.autherName.length >=10){
-  //   detailInfo.autherName.substr(0, 6)
-
-  //  }else{ "no" }
-
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className="container total ">
-      <div className="title container">
+    <div className="container total">
+      <div className="title">
         <div className="cover">
-
-        {detailInfo.coverImageUri ?
-          <img
-            src={`http://haeji.mawani.kro.kr:8181/image/cover/${detailInfo.coverImageUri}`}
-            //  onError={(event) => event.target.style.display = 'none'}
-            className="booktitle container"
-            alt="책표지"
-          ></img> : ""}
+          {detailInfo.coverImageUri ? (
+            <img
+              src={`http://haeji.mawani.kro.kr:8181/image/cover/${detailInfo.coverImageUri}`}
+              className="booktitle"
+              alt="책표지"
+            ></img>
+          ) : (
+            ""
+          )}
         </div>
-
-        <ul className="list-group info container">
-          <div className="event container">
-
+        <ul className="info">
+          <div className="event">
             <button type="button" className="btn btn-outline-light w-11 p-1">
               이벤트
             </button>
@@ -53,70 +34,79 @@ const Goodinfo = (props) => {
               무료배송
             </button>
           </div>
-          <h2 className="titleinfo container">
-            {detailInfo.title ? detailInfo.title : "No title"}
-          </h2>
-          <p className="minigray container">
-            {detailInfo.autherName} |{" "}
-            {detailInfo.publisherName
-              ? detailInfo.publisherName
-              : "No publisherName"}{" "}
-            | {detailInfo.regDt}
-          </p>
+          <div className="detail-bookinfo">
+            <h2 className="titleinfo">{detailInfo.title ? detailInfo.title : "No title"}</h2>
+            <p className="minigray">
+              <span>{detailInfo.autherName} 저자(글)</span>
+              <span>
+                {detailInfo.publisherName ? detailInfo.publisherName : "No publisherName"} ·{" "}
+                {detailInfo.regDt}
+              </span>
+            </p>
+          </div>
           <p className="discount">
-            {detailInfo.discount ? detailInfo.discount : "No discount"}{"  "}
+            {detailInfo.discount ? detailInfo.discount : "No discount"}
+            {"  "}
             <span className="discountprice">
               {detailInfo.discountPrice
-                ? detailInfo.discountPrice
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                : "NO discount"}{"  "}
+                ? detailInfo.discountPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                : "NO discount"}
+              {"  "}
             </span>
             <span className="saleprice">
               {detailInfo.price
-                ? detailInfo.price
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                ? detailInfo.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
                 : "NO price"}
             </span>
           </p>
           <div className="point">
             <span>적립</span>
-            <span>{detailInfo.point ? detailInfo.point.toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : "no point"}p</span>
-          </div>
-          <div className="point">
-            <span>배송정보</span>
-            <span>
-              {detailInfo.delivery ? detailInfo.delivery : "No delivery"}
+            <span className="point-price">
+              {detailInfo.point
+                ? detailInfo.point.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                : "no point"}
+              p
             </span>
           </div>
           <div className="point">
+            <span>배송정보</span>
+            <span>{detailInfo.delivery ? detailInfo.delivery : "No delivery"}</span>
+          </div>
+          <div className="point">
             <span>리뷰평점</span>
-            <span>{detailInfo.score ? detailInfo.score : "0"}</span>
+            <span>
+              <img className="clover-img" src={"/ico_clover.png"} />{" "}
+              {detailInfo.score ? detailInfo.score : "0"}
+            </span>
           </div>
         </ul>
-        <div></div>
       </div>
 
-      <div className="container introdetail">
+      <div className="container detail-menu">
+        <p className="detail-menu-item">상품 정보</p>
+      </div>
+
+      <div className="introdetail">
         {detailInfo.introImageUri ? (
           <img
             src={`http://haeji.mawani.kro.kr:8181/image/intro/${detailInfo.introImageUri}`}
             // onError= {(event) => event.target.style.display = 'none'}
-            className="bookdetail container"
+            className="bookdetail"
             alt="상세설명"
           ></img>
         ) : (
           ""
         )}
 
-        <p className="bookinfotitle container">책소개</p>
+        <p className="bookinfotitle">책소개</p>
         <p className="bookinfo"> {detailInfo.textIntroduce}</p>
       </div>
       <div className="scroll__container">
-<button id="top" onClick={scrollToTop} type="button" > Top</button>
-</div>
+        <button id="top" onClick={scrollToTop} type="button">
+          {" "}
+          Top
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,12 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <div className="text-center">
-        <Link to="/Home">
-          <img src="/photo/img_logo_kyobo@2x.png" className="logo" alt="이미지"></img>
-        </Link>
-        <p className="subTitle">컴퓨터/IT베스트셀러</p>
-        <span className="line"></span>
-      </div>
+    <div className="container header">
+      <Link to="/Home">
+        <img src="/photo/img_logo_kyobo@2x.png" className="logo" alt="이미지"></img>
+
+        <p className="subTitle text-center">컴퓨터/IT베스트셀러</p>
+      </Link>
     </div>
   );
 };
